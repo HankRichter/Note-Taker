2a842a83eca7b32fe6f42984df687956b17ed6fc
Hank Richter <120127039+HankRichter@users.noreply.github.com>
2023-04-23 21:23:14 -0400

    Assigning global variables

--- server.js
+++ server.js
@@ -2,3 +2,8 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 const { v4: uuidv4 } = require("uuid");
+const app = express();
+let db = JSON.parse(fs.readFileSync("db/db.json"));
+
+const PORT = process.env.PORT || 3000;
+
