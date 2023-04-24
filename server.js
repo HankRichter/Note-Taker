d20fc08bdfee1e493cebee4e8d38006d0db5eb94
Hank Richter <120127039+HankRichter@users.noreply.github.com>
2023-04-23 21:29:50 -0400

    Route for getting all notes.

--- server.js
+++ server.js
@@ -16,3 +16,6 @@ app.get("/", (req, res) => {
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
+app.get("/api/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/db/db.json"));
+});
